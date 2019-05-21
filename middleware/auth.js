eea49a98348ea69075ffcf2ded9d9ebdd1abadfa
willehpeh <will_alexander@me.com>
2019-05-21 09:38:43 +0200

    Updated auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
+const dbConfig = require('../config/db');
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, dbConfig.jwtSecret);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
       next();
     }
-  } catch {
+  } catch(error) {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: error || 'Invalid request!'
     });
   }
 };
